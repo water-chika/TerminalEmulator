75fe159b6a17fd0f579ac370750c1f0cb84c8435
Water Chika <Water Chika>
2024-01-31 23:03:26 -0800

    Add depth buffer create function

--- terminal_emulator.cpp
+++ terminal_emulator.cpp
@@ -6,6 +6,44 @@
 #include <iostream>
 #include <cassert>
 #include <array>
+#include <span>
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+
+template<class T>
+class from_0_count_n {
+public:
+	struct T_ite {
+		using difference_type = int64_t;
+		using value_type = T;
+		using pointer = T*;
+		using reference = T&;
+		using iterator_category = std::random_access_iterator_tag;
+		using iterator_concept = std::contiguous_iterator_tag;
+		constexpr T operator*() const noexcept { return value; }
+		constexpr T_ite& operator++() noexcept { ++value; return *this; }
+		constexpr T_ite operator++(int) noexcept { return T_ite{ value++ }; }
+		constexpr T_ite& operator--() noexcept { --value; return *this; }
+		constexpr T_ite operator--(int) noexcept { return T_ite{ value-- }; }
+		constexpr bool operator==(T_ite rhs) const noexcept{
+			return value == rhs.value;
+		}
+		constexpr int64_t operator-(T_ite rhs) const noexcept {
+			return static_cast<int64_t>(value) - rhs.value;
+		}
+		T value;
+	};
+	constexpr from_0_count_n(T count) : m_count{ count } {}
+	constexpr T size() noexcept { return m_count; }
+	constexpr T_ite begin() noexcept { return T_ite{ 0 }; }
+	constexpr T_ite end() noexcept { return T_ite{ m_count }; }
+private:
+	T m_count;
+};
+
+template<class T>
+inline constexpr bool std::ranges::enable_borrowed_range<from_0_count_n<T>> = true;
 
 namespace terminal {
 	auto create_instance() {
@@ -46,6 +84,50 @@ namespace terminal {
 		vk::DeviceCreateInfo deviceCreateInfo({}, deviceQueueCreateInfo, deviceLayers, deviceExtensions);
 
 		return physical_device.createDevice(deviceCreateInfo);
+	}
+	auto select_depth_image_tiling(vk::PhysicalDevice physical_device, vk::Format format) {
+		vk::FormatProperties format_properties = physical_device.getFormatProperties(format);
+		if (format_properties.linearTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) {
+			return vk::ImageTiling::eLinear;
+		}
+		else if (format_properties.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) {
+			return vk::ImageTiling::eOptimal;
+		}
+		else {
+			throw std::runtime_error{ "DepthStencilAttachment is not supported for this format" };
+		}
+	}
+	auto create_image(vk::Device device, vk::ImageType type, vk::Format format, vk::Extent2D extent, vk::ImageTiling tiling, vk::ImageUsageFlags usages) {
+		vk::ImageCreateInfo create_info{ {}, type, format, vk::Extent3D{extent, 1}, 1, 1, vk::SampleCountFlagBits::e1, tiling, usages };
+		return device.createImage(create_info);
+	}
+	template<class T, class B>
+	bool contain_bit(T bitfield, B bit) {
+		return (bitfield & bit) == bit;
+	}
+
+	auto select_memory_type(vk::PhysicalDevice physical_device, vk::Device device, vk::MemoryRequirements requirements) {
+		auto memory_properties = physical_device.getMemoryProperties();
+		auto type_bits = requirements.memoryTypeBits;
+		return *std::ranges::find_if(from_0_count_n(memory_properties.memoryTypeCount), [&memory_properties, type_bits](auto i) {
+			if ((type_bits & (1 << i)) &&
+				contain_bit(memory_properties.memoryTypes[i].propertyFlags, vk::MemoryPropertyFlagBits::eDeviceLocal)) {
+				return true;
+			}
+			return false;
+			});
+	}
+	auto allocate_device_memory(vk::PhysicalDevice physical_device, vk::Device device, vk::Image image) {
+
+		auto memory_requirements = device.getImageMemoryRequirements(image);
+		auto type_index = select_memory_type(physical_device, device, memory_requirements);
+		return device.allocateMemory(vk::MemoryAllocateInfo{ memory_requirements.size, type_index });
+	}
+	auto create_depth_buffer(vk::PhysicalDevice physical_device, vk::Device device, uint32_t width, uint32_t height) {
+		const vk::Format format = vk::Format::eD16Unorm;
+		vk::ImageTiling tiling = select_depth_image_tiling(physical_device, format);
+		auto image = create_image(device, vk::ImageType::e2D, format, vk::Extent2D{ width, height }, tiling, vk::ImageUsageFlagBits::eDepthStencilAttachment);
+		auto memory = allocate_device_memory(physical_device, device, image);
 	}
 }
 
