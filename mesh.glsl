dc13abade5c4931ba7fe9805f4d980569f3f4198
Water Chika <water_chika@outlook.com>
2024-02-04 01:10:16 +0800

    refine mesh.glsl

--- mesh.glsl
+++ mesh.glsl
@@ -8,10 +8,11 @@ void main(){
     vec2 pos = gl_LocalInvocationID.xy * 0.1;
     vec2 pos1 = pos + vec2(1,0)*0.1;
     vec2 pos2 = pos + vec2(0,1)*0.1;
-    gl_MeshVerticesEXT[3*gl_LocalInvocationIndex + 0].gl_Position = vec4(pos,0,1);
-    gl_MeshVerticesEXT[3*gl_LocalInvocationIndex + 1].gl_Position = vec4(pos1,0,1);
-    gl_MeshVerticesEXT[3*gl_LocalInvocationIndex + 2].gl_Position = vec4(pos2,0,1);
-    gl_PrimitiveTriangleIndicesEXT[gl_LocalInvocationIndex + 0] = uvec3(3*gl_LocalInvocationIndex,3*gl_LocalInvocationIndex+1,3*gl_LocalInvocationIndex+2);
+    uint vertex_start_index = 3*gl_LocalInvocationIndex;
+    gl_MeshVerticesEXT[vertex_start_index + 0].gl_Position = vec4(pos,0,1);
+    gl_MeshVerticesEXT[vertex_start_index + 1].gl_Position = vec4(pos1,0,1);
+    gl_MeshVerticesEXT[vertex_start_index + 2].gl_Position = vec4(pos2,0,1);
+    gl_PrimitiveTriangleIndicesEXT[gl_LocalInvocationIndex] = uvec3(vertex_start_index,vertex_start_index+1,vertex_start_index+2);
     uint work_group_size = gl_WorkGroupSize.x;
     SetMeshOutputsEXT(3*work_group_size, work_group_size);
 }
