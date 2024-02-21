5924d010f799c199865ade1a271a753de5154250
Water Chika <water_chika@outlook.com>
2024-02-21 19:17:42 +0800

    Add font file search paths.

--- font_loader.hpp
+++ font_loader.hpp
@@ -30,13 +30,21 @@ public:
 		if (FT_Init_FreeType(&m_library)) {
 			throw std::runtime_error{ "failed to initialize font library" };
 		}
-		auto os_font_paths = std::map<os, std::string>{};
-		os_font_paths.emplace(os::eWindows, "C:/Windows/Fonts/consola.ttf");
-		os_font_paths.emplace(os::eLinux,   "/usr/share/fonts/gnu-free/FreeMono.otf");
-		auto font_path = os_font_paths[build_info::runtime_os];
-		if (FT_New_Face(m_library, font_path.c_str(), 0, &m_face)) {
-			throw std::runtime_error{ "failed to open font file" };
-		}
+		auto os_font_paths = std::map<os, std::vector<std::string>>{
+            {os::eWindows, {}},
+            {os::eLinux, {}},
+        };
+		os_font_paths[os::eWindows].emplace_back("C:/Windows/Fonts/consola.ttf");
+        os_font_paths[os::eLinux].emplace_back("/usr/share/fonts/gnu-free/FreeMono.otf");
+		os_font_paths[os::eLinux].emplace_back("/usr/share/fonts/truetype/freefont/FreeMono.ttf");
+		const auto& font_paths = os_font_paths[build_info::runtime_os];
+        if (font_paths.end() ==std::find_if(
+                font_paths.begin(), font_paths.end(),
+                [&library=m_library, &face=m_face](auto& font_path) {
+                    return 0 == FT_New_Face(library, font_path.c_str(), 0, &face);
+                })) {
+            throw std::runtime_error{ "failed to open font file" };
+        }
 		if (FT_Set_Char_Size(m_face, 0, 16 * 64, 512, 512)) {
 			throw std::runtime_error{ "failed to set font size" };
 		}
@@ -48,6 +56,9 @@ public:
 		}
 	}
 	void render_char(char c) {
+		if (c == '\0') {
+			c = ' ';
+		}
 		auto glyph_index = FT_Get_Char_Index(m_face, c);
 		assert(glyph_index != 0);
 		if (FT_Load_Glyph(m_face, glyph_index, FT_LOAD_DEFAULT)) {
