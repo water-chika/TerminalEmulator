ca04721dbd50f57f21308d97d43f5592ea2939eb
Water Chika <water_chika@outlook.com>
2024-02-23 03:22:53 -0800

    Move windows spirv reader's calls to generated header

--- spirv_reader.hpp
+++ spirv_reader.hpp
@@ -8,16 +8,8 @@
 
 class spirv_file : protected spirv_reader_os_member{
 public:
-    spirv_file(std::filesystem::path path) {
-#ifdef WIN32
-        hFile = CreateFileW(path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-        assert(hFile != INVALID_HANDLE_VALUE);
-        hMapping = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
-        assert(hMapping != INVALID_HANDLE_VALUE);
-        mmaped_ptr = MapViewOfFile(hMapping, FILE_MAP_READ, 0, 0, 0);
-        assert(mmaped_ptr != nullptr);
-        m_size = GetFileSize(hFile, NULL);
-#endif
+    spirv_file(std::filesystem::path path)
+        : spirv_reader_os_member{ path} {
 #ifdef __unix__
         m_file_descriptor = open(path.c_str(), O_RDONLY);
         m_size = lseek(m_file_descriptor, 0, SEEK_END);
@@ -27,11 +19,6 @@ public:
     spirv_file(const spirv_file& file) = delete;
     spirv_file(spirv_file&& file) = delete;
     ~spirv_file() {
-#ifdef WIN32
-        UnmapViewOfFile(mmaped_ptr);
-        CloseHandle(hMapping);
-        CloseHandle(hFile);
-#endif
 #ifdef __unix__
         munmap(mmaped_ptr, m_size);
         close(m_file_descriptor);
